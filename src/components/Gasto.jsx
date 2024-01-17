6b15e8425c9b86db5bdc277e983dc225ff31fd16
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-01-17 16:34:29 -0600

    add img.png a cada gasto de forma dinamica, add f: dictionaryIcons con la importacion de cada una de las imagenes

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,28 +1,49 @@
 import React from 'react'
 import { formatDate } from '../helpers';
 
-const Gasto = ({gasto}) => {
-    // desestructuracion del prop de gasto
-    const{ nombre, categoria, cantidad, id, date } = gasto;
-  return (
-    <div className='gasto sombra'>
-        <div className="contenido-gasto">
-            <div className="descripcion-gasto">
+import healt from '../img/heart.png'
+import eat from '../img/food.png'
+import pets from '../img/vet.png'
+import train from '../img/train.png'
+import subsciptions from '../img/subscribe.png'
+import other from '../img/application.png'
 
-                <p className='categoria'>
-                    {categoria}
-                </p>
-                <p className='nombre-gasto'>
-                    {nombre}
-                </p>
-                <p className='fecha-gasto'>
-                    Fecha de registro:{' '}
-                    <span>{formatDate(date)}</span>
-                </p>
+
+const dictionaryIcons = {
+    salud: healt,
+    alimentos: eat,
+    mascotas: pets,
+    trasporte: train,
+    subscipciones: subsciptions,
+    otros: other
+
+}
+
+
+const Gasto = ({ gasto }) => {
+    // desestructuracion del prop de gasto
+    const { nombre, categoria, cantidad, id, date } = gasto;
+    return (
+        <div className='gasto sombra'>
+            <div className="contenido-gasto">
+                <img src={dictionaryIcons[categoria]} alt='icon expense' />
+                <div className="descripcion-gasto">
+
+                    <p className='categoria'>
+                        {categoria}
+                    </p>
+                    <p className='nombre-gasto'>
+                        {nombre}
+                    </p>
+                    <p className='fecha-gasto'>
+                        Fecha de registro:{' '}
+                        <span>{formatDate(date)}</span>
+                    </p>
+                </div>
             </div>
+            <p className='cantidad-gasto'>${cantidad}**</p>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Gasto
