8561575af8613c7437440aa538cadf29fc3f77d7
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-01-17 17:55:00 -0600

    Envolviendo el div de gasto con SwipeableList & SwipeableListItem, add f: leadingActions & trailingActions

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { formatDate } from '../helpers';
-
+import { LeadingActions, SwipeableList, SwipeableListItem, SwipeAction, TrailingActions } from 'react-swipeable-list'
+// hoja de estilos de la libreria 
+import 'react-swipeable-list/dist/styles.css'
 import healt from '../img/heart.png'
 import eat from '../img/food.png'
 import pets from '../img/vet.png'
@@ -23,26 +25,41 @@ const dictionaryIcons = {
 const Gasto = ({ gasto }) => {
     // desestructuracion del prop de gasto
     const { nombre, categoria, cantidad, id, date } = gasto;
-    return (
-        <div className='gasto sombra'>
-            <div className="contenido-gasto">
-                <img src={dictionaryIcons[categoria]} alt='icon expense' />
-                <div className="descripcion-gasto">
 
-                    <p className='categoria'>
-                        {categoria}
-                    </p>
-                    <p className='nombre-gasto'>
-                        {nombre}
-                    </p>
-                    <p className='fecha-gasto'>
-                        Fecha de registro:{' '}
-                        <span>{formatDate(date)}</span>
-                    </p>
+    const leadingActions = () => {
+        console.log('Editar Gasto')
+    }
+    const trailingActions = () => {
+        console.log('Eliminar Gasto')
+    }
+
+    return (
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions}
+                trailingActions={trailingActions}
+            >
+                <div className='gasto sombra'>
+                    <div className="contenido-gasto">
+                        <img src={dictionaryIcons[categoria]} alt='icon expense' />
+                        <div className="descripcion-gasto">
+
+                            <p className='categoria'>
+                                {categoria}
+                            </p>
+                            <p className='nombre-gasto'>
+                                {nombre}
+                            </p>
+                            <p className='fecha-gasto'>
+                                Fecha de registro:{' '}
+                                <span>{formatDate(date)}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <p className='cantidad-gasto'>${cantidad}**</p>
                 </div>
-            </div>
-            <p className='cantidad-gasto'>${cantidad}**</p>
-        </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
