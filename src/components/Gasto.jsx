43e81d51a70fe18c2e30b07d43ca216f44f41c6a
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-01-17 15:41:54 -0600

    f: formatDate para dar formato a la fecha ya que se va a mostrar en el gasto, cuando este sea generado.

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { formatDate } from '../helpers';
 
 const Gasto = ({gasto}) => {
     // desestructuracion del prop de gasto
-    const{ nombre, categoria, cantidad, id } = gasto;
+    const{ nombre, categoria, cantidad, id, date } = gasto;
   return (
     <div className='gasto sombra'>
         <div className="contenido-gasto">
@@ -14,7 +15,10 @@ const Gasto = ({gasto}) => {
                 <p className='nombre-gasto'>
                     {nombre}
                 </p>
-                <p></p>
+                <p className='fecha-gasto'>
+                    Fecha de registro:{' '}
+                    <span>{formatDate(date)}</span>
+                </p>
             </div>
         </div>
     </div>
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,20 @@
 
-   //Snipet para generar un id unico.
+    //Snipet para generar un id unico.
 export const generarId = () => {
     //generar un numero random 
     const random = Math.random().toString(36).substring(2)
     const fecha = Math.random().toString(36)
 
     return random + fecha
+}
+    // formateo de fecha para cuando esta funcion se mande llamar en Gasto.jsx
+export const formatDate = (fecha) => {
+    const newDate = new Date(fecha);
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return newDate.toLocaleDateString('es-ES', options)
 }
