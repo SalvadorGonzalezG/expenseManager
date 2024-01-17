f441bc70dbcae4ada3da9cdd8f17e2e71114b4ea
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-01-16 21:42:37 -0600

    add formulario in Modal & select options

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -20,7 +20,42 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             />
         </div>
         <form className={`formulario ${animarModal ? "animar": 'cerrar' }`}>
-            <legend>Nuego Gasto.</legend>
+            <legend>Nuevo Gasto.</legend>
+
+            <div className='campo'>
+                <label htmlFor="nombre">Nombre del Gasto.</label>
+
+                <input 
+                  id='nombre'
+                  type="text"
+                  placeholder='Añade el nombre de tu gasto'
+                  />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor="cantidad">Cantidad:</label>
+
+                <input 
+                  id='cantidad'
+                  type="number"
+                  placeholder='Cantidad total del gasto.'
+                  />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor="categoria">Categoria.</label>
+
+                <select id="categoria">
+                  <option value="">-- Selecciona --</option>
+                  <option value="salud">Salud.</option>
+                  <option value="alimentos">Alimentos.</option>
+                  <option value="mascotas">Mascotas.</option>
+                  <option value="trasporte">Transporte.</option>
+                  <option value="subscipciones">Subscripciones.</option>
+                  <option value="otros">Otros Gastos</option>
+                </select>
+            </div>
+            <input type="submit" value="añadir gasto." />
         </form>
 
     </div>
